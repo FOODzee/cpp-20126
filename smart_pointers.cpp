47c0c2b442facb2950e99c1899383c1f82b8eae0
Igor Jorch <jorch.igor@huawei.com>
2021-11-22 02:02:50 +0800

    add a demo for smart pointers

---
+++ smart_pointers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct test {
+    int i;
+    test(int i) : i(i) {}
+
+    ~test() {
+        std::cout << "  ~test  " << i;
+    }
+};
+
+int main() {
+    auto sp = std::make_shared<test>(1);
+    {
+        std::shared_ptr<test> spc = sp;
+
+        std::cout << "15: " << sp.use_count() << std::endl;
+    }
+    std::cout << "17: " << sp.use_count() << std::endl;
+    sp = std::make_shared<test>(2);
+    std::cout << "17: " << sp.use_count() << std::endl;
+    return 0;
+}
